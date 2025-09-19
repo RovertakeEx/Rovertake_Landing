1d22bcabe196efadfda949d9363376b75a549b9d
Rifnaz <hanifarifnazz@gmail.com>
2025-09-19 19:08:21 +0530

    index css minified

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  import.meta.env.DEV 
+  ? (
+     <StrictMode>
+      <App />
+    </StrictMode>
+  ) 
+  : (<App />)
+);
